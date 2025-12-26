ff6a68ccb8dc08ccc5bfbddfe526bc07b534e7b7
Neeraj <shukla7071@gmail.com>
2025-12-26 12:09:09 +0530

    onr more  try

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-import React from "react";
 
-function App() {
+import React from "react";
+import './../styles/App.css';
+
+const App = () => {
   return (
-    <div >
-      <h1 >Welcome to my website</h1>
-      <p>
-        This is the content of my website.
-      </p>
-      <p>
-        © 2023 My Website. All rights reserved.
-      </p>
+    <div>
+        {/* Do not remove the main div */}
+        <h1>Welcome to my website</h1>
+        <p>This is the content of website</p>
+        <p> © 2023 My Website. All rights reserved.</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
