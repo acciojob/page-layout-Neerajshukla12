2f52f7264adbc12849f199801df87dc2eb3d47bc
Neeraj <shukla7071@gmail.com>
2025-12-26 12:00:53 +0530

    second try

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
-
 import React from "react";
-import './../styles/App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <h1>Welcome to my website</h1>
-        <p>This is the content of website</p>
-        <p> © 2023 My Website. All rights reserved.</p>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Welcome to my website</h1>
+
+      <p style={styles.text}>
+        This is the content of my website.
+      </p>
+
+      <p style={styles.footer}>
+        © 2023 My Website. All rights reserved.
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+const styles = {
+  container: {
+    padding: "40px",
+    fontFamily: "Times New Roman, serif",
+  },
+  heading: {
+    fontSize: "48px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+  },
+  text: {
+    fontSize: "22px",
+    marginBottom: "20px",
+  },
+  footer: {
+    fontSize: "20px",
+  },
+};
+
+export default App;
