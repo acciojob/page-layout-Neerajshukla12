06e318e94e968944655dcb9e9d14f1b1670cef8c
Neeraj <shukla7071@gmail.com>
2025-12-26 11:44:55 +0530

    first question done

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Welcome to my website</h1>
+        <p>This is the content of website</p>
+        <h6> © 2023 My Website. All rights reserved.</h6>
     </div>
   )
 }
