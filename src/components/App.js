68482bb780a4eca5dd77bad156d593f266950156
Neeraj <shukla7071@gmail.com>
2025-12-26 12:07:04 +0530

    onr more  try

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,16 @@ import React from "react";
 
 function App() {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.heading}>Welcome to my website</h1>
-
-      <p style={styles.text}>
+    <div >
+      <h1 >Welcome to my website</h1>
+      <p>
         This is the content of my website.
       </p>
-
-      <p style={styles.footer}>
+      <p>
         © 2023 My Website. All rights reserved.
       </p>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    padding: "40px",
-    fontFamily: "Times New Roman, serif",
-  },
-  heading: {
-    fontSize: "48px",
-    fontWeight: "bold",
-    marginBottom: "20px",
-  },
-  text: {
-    fontSize: "22px",
-    marginBottom: "20px",
-  },
-  footer: {
-    fontSize: "20px",
-  },
-};
 
 export default App;
